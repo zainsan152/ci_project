cca5b423c1dbedad1eb1d53c05354a847d664269
Zain Ul Abideen <zain.san.su.152@gmail.com>
2020-04-15 09:30:27 -0700

    login model and model connect with controller

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -1,30 +1,43 @@
-<?php 
-
-
+<?php
 /**
-  * 
-  */
- class admin extends My_Controller
- {
- 	
- 	
+*
+*/
+class admin extends My_Controller
+{
+	
+	
 	public function index()
 	{
 		$this->load->library('form_validation');
 		$this->form_validation->set_rules('uname' , 'User Name' , 'required|alpha');
 		$this->form_validation->set_rules('pass' , 'Password' , 'required|max_length[12]');
 		$this->form_validation->set_error_delimiters('<div class = "text-danger">' , '</div>');
-
 		if ($this->form_validation->run())
 		{
-			echo "Validation Successfull";
+			$name = $this->input->post('uname');
+			$password = $this->input->post('pass');
+			$this->load->model('loginmodel');
+			$id = $this->loginmodel->isvalidate($name , $password);
+			//echo $id;
+			if($id)
+			{
+				$this->session->set_userdata('id' , $id);
+				/*$this->load->view('admin/dashboard');*/
+				return redirect('admin/welcome');
+			}
+			else
+			{
+				echo "Not matched";
+			}
 		}
 		else
 		{
-			$this->load->view('Users/articleList');
+			$this->load->view('Admin/Login');
 		}
 	}
- }
-
-
-  ?>
+	public function welcome()
+	{
+		$this->load->view('admin/dashboard');
+	}
+}
+?>
